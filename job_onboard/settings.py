6e03d6a2371c27086ff3c604bba432cf06212bd0
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-31 04:26:26 +0000

    deploy changes

--- job_onboard/settings.py
+++ job_onboard/settings.py
@@ -23,9 +23,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ydn&)8rppyx=g5qcoyhec6ym-np!s9jc99o5*-_lli9mgb6)gr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'joboonboard.pythonanywhere.com',
+    'www.joboonboard.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
+]
+
+# Security settings for production
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
 
 
 # Application definition
@@ -37,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'social_django',
     'job_portal',
 ]
@@ -50,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
     'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
